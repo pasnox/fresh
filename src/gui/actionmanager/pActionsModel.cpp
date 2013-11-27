e9ddbc5b3043561733c62a32b67c1d12dce37efc
Nox P@sNox <pasnox@gmail.com>
2013-11-27 20:14:16 +0100

    Qt5 fix in debug build

--- src/gui/actionmanager/pActionsModel.cpp
+++ src/gui/actionmanager/pActionsModel.cpp
@@ -55,11 +55,11 @@ int pActionsModel::columnCount( const QModelIndex& parent ) const
 QVariant pActionsModel::data( const QModelIndex& index, int role ) const
 {
     QAction* action = this->action( index );
-    
+
     if ( !action ) {
         return QVariant();
     }
-    
+
     switch ( role ) {
         case Qt::DecorationRole:
             switch ( index.column() ) {
@@ -68,7 +68,7 @@ QVariant pActionsModel::data( const QModelIndex& index, int role ) const
                 default:
                     break;
             }
-            
+
             break;
         case Qt::DisplayRole:
         case Qt::ToolTipRole:
@@ -80,28 +80,28 @@ QVariant pActionsModel::data( const QModelIndex& index, int role ) const
                 case pActionsModel::DefaultShortcut:
                     return defaultShortcut( action ).toString( QKeySequence::NativeText );
             }
-            
+
             break;
-        
+
         case Qt::FontRole: {
             QFont font = action->font();
-            
+
             if ( action->menu() ) {
                 font.setBold( true );
             }
-            
+
             return font;
         }
-        
+
         /*case Qt::BackgroundRole:
             return action->menu() ? QBrush( QColor( 0, 0, 255, 20 ) ) : QVariant();*/
-        
+
         case pActionsModel::MenuRole:
             return QVariant::fromValue( action->menu() );
         case pActionsModel::ActionRole:
             return QVariant::fromValue( action );
     }
-    
+
     return QVariant();
 }
 
@@ -109,13 +109,13 @@ QModelIndex pActionsModel::index( int row, int column, const QModelIndex& parent
 {
     QAction* action = this->action( parent );
     QList<QAction*> actions = children( action );
-    
+
     if ( row < 0 || row >= actions.count()
         || column < 0 || column >= mColumnCount
         || ( parent.column() != 0 && parent.isValid() ) ) {
         return QModelIndex();
     }
-    
+
     return createIndex( row, column, actions.at( row ) );
 }
 
@@ -158,25 +158,25 @@ QVariant pActionsModel::headerData( int section, Qt::Orientation orientation, in
                         return tr( "Default Shortcut" );
                 }
             }
-            
+
             break;
         }
-        
+
         default:
             break;
     }
-    
+
     return QAbstractItemModel::headerData( section, orientation, role );
 }
 
 QModelIndex pActionsModel::index( QAction* action, int column ) const
 {
     const QString path = this->path( action );
-    
+
     if ( !mActions.contains( path ) ) {
         return QModelIndex();
     }
-    
+
     QAction* parentAction = parent( action );
     const int row = children( parentAction ).indexOf( action );
     return row != -1 ? createIndex( row, column, mActions[ path ] ) : QModelIndex();
@@ -200,36 +200,36 @@ QAction* pActionsModel::action( const QString& path ) const
 void pActionsModel::clear()
 {
     const int count = rowCount();
-    
+
     if ( count == 0 ) {
         return;
     }
-    
+
     beginRemoveRows( QModelIndex(), 0, count -1 );
     qDeleteAll( mChildren.value( 0 ) );
     mChildren.clear();
     mActions.clear();
     endRemoveRows();
-    
+
     emit actionsCleared();
 }
 
 bool pActionsModel::addAction( const QString& _path, QAction* action )
 {
     Q_ASSERT( action );
-    
+
     if ( !action || !path( action ).isEmpty() || this->action( _path ) ) {
         return false;
     }
-    
+
     const QString path = cleanPath( _path );
     const QString subPath = path.section( '/', 0, -2 );
     QAction* parentAction = createCompletePathNode( subPath );
-    
+
     if ( !parentAction ) {
         return false;
     }
-    
+
     const int row = children( parentAction ).count();
     insertAction( path, action, parentAction, row );
     return true;
@@ -238,12 +238,12 @@ bool pActionsModel::addAction( const QString& _path, QAction* action )
 QAction* pActionsModel::addAction( const QString& path, const QString& text, const QIcon& icon )
 {
     QAction* action = new QAction( icon, text, this );
-    
+
     if ( !addAction( path, action ) ) {
         delete action;
         return 0;
     }
-    
+
     return action;
 }
 
@@ -251,27 +251,27 @@ QAction* pActionsModel::addSeparator( const QString& path, const QString& text )
 {
     QAction* action = new QAction( text, this );
     action->setSeparator( true );
-    
+
     if ( !addAction( path, action ) ) {
         delete action;
         return 0;
     }
-    
+
     return action;
 }
 
 QAction* pActionsModel::addMenu( const QString& path, const QString& text, const QIcon& icon )
 {
     QAction* action = createCompletePathNode( path );
-    
+
     if ( action ) {
         action->setIcon( icon );
-        
+
         if ( !text.isEmpty() ) {
             action->setText( text );
         }
     }
-    
+
     return action;
 }
 
@@ -295,16 +295,16 @@ bool pActionsModel::removeMenu( QAction* action, bool removeEmptyPath )
     if ( !action ) {
         return false;
     }
-    
+
     QAction* parentAction = parent( action );
     const int row = children( parentAction ).indexOf( action );
-    
+
     removeAction( action, parentAction, row );
-    
+
     if ( removeEmptyPath ) {
         removeCompleteEmptyPathNode( parentAction );
     }
-    
+
     return true;
 }
 
@@ -337,7 +337,7 @@ void pActionsModel::setDefaultShortcut( QAction* action, const QKeySequence& sho
 {
     if ( action ) {
         action->setProperty( DEFAULT_SHORTCUT_PROPERTY, shortcut );
-        
+
         if ( action->shortcut().isEmpty() ) {
             setShortcut( action, shortcut );
         }
@@ -358,13 +358,13 @@ bool pActionsModel::setShortcut( QAction* action, const QKeySequence& shortcut, 
                     if ( error ) {
                         *error = tr( "Can't set shortcut, it's already used by action '%1'." ).arg( cleanText( a->text() ) );
                     }
-                    
+
                     return false;
                 }
             }
         }
     }
-    
+
     action->setShortcut( shortcut );
     return true;
 }
@@ -381,15 +381,15 @@ QString pActionsModel::cleanPath( const QString& path )
         //.remove( ' ' )
         .trimmed()
         ;
-    
+
     while ( data.startsWith( '/' ) ) {
         data.remove( 0, 1 );
     }
-    
+
     while ( data.endsWith( '/' ) ) {
         data.chop( 1 );
     }
-    
+
     return data;
 }
 
@@ -397,10 +397,10 @@ QString pActionsModel::cleanPath( const QString& path )
 void pActionsModel::debugInternals()
 {
     foreach ( QAction* parent, mChildren.keys() ) {
-        qWarning() << ( parent ? parent->text() : "ROOT" ).toAscii().constData() << parent;
-        qWarning() << QString( 1, '\t' ).toAscii().constData() << mChildren.value( parent );
+        qWarning() << ( parent ? parent->text() : "ROOT" ).toLocal8Bit().constData() << parent;
+        qWarning() << QString( 1, '\t' ).toLocal8Bit().constData() << mChildren.value( parent );
     }
-    
+
     qWarning() << mActions.keys();
 }
 
@@ -420,18 +420,18 @@ bool pActionsModel::isValid( const QModelIndex& index ) const
     if ( !index.isValid() || index.row() < 0 || index.column() < 0 || index.column() >= mColumnCount ) {
         return false;
     }
-    
+
     QAction* action = static_cast<QAction*>( index.internalPointer() );
     QAction* parentAction = parent( action );
-    
+
     if ( !action ) {
         return false;
     }
-    
+
     if ( index.row() >= children( parentAction ).count() ) {
         return false;
     }
-    
+
     return true;
 }
 
@@ -447,20 +447,20 @@ QString pActionsModel::cleanText( const QString& text ) const
 void pActionsModel::insertAction( const QString& path, QAction* action, QAction* parent, int row )
 {
     Q_ASSERT( row != -1 );
-    
+
     QObject* p = parent;
-    
+
     if ( !p ) {
         p = this;
     }
-    
+
 #ifndef QT_NO_DEBUG
     if ( mDebugging ) {
         qWarning() << path << action << parent << row << index( parent ) << index( rowCount() -1, 0 );
         qWarning() << index( parent ).data() << index( rowCount() -1, 0 ).data();
     }
 #endif
-    
+
     beginInsertRows( index( parent ), row, row );
     action->setObjectName( QString( path ).replace( "/", "_" ) );
     action->setParent( p );
@@ -475,7 +475,7 @@ void pActionsModel::insertAction( const QString& path, QAction* action, QAction*
     connect( action, SIGNAL( changed() ), this, SLOT( actionChanged() ) );
     connect( action, SIGNAL( destroyed( QObject* ) ), this, SLOT( actionDestroyed( QObject* ) ) );
     endInsertRows();
-    
+
     emit actionInserted( action );
 }
 
@@ -484,7 +484,7 @@ void pActionsModel::cleanTree( QAction* action, QAction* parent )
     foreach ( QAction* a, mChildren.value( action  ) ) {
         cleanTree( a, action );
     }
-    
+
     QList<QAction*>& parentChildren = mChildren[ parent ];
     parentChildren.removeAt( parentChildren.indexOf( action ) );
     mChildren.remove( action );
@@ -494,50 +494,50 @@ void pActionsModel::cleanTree( QAction* action, QAction* parent )
 void pActionsModel::removeAction( QAction* action, QAction* parent, int row )
 {
     Q_ASSERT( row != -1 );
-    
+
     beginRemoveRows( index( parent ), row, row );
     if ( parent ) {
         parent->menu()->removeAction( action );
     }
     cleanTree( action, parent );
     endRemoveRows();
-    
+
     emit actionRemoved( action );
-    
+
     action->deleteLater();
 }
 
 QAction* pActionsModel::createCompletePathNode( const QString& path )
 {
     QAction* action = mActions.value( path );
-    
+
     if ( action ) {
         return action->menu() ? action : 0;
     }
-    
+
     const int separatorCount = path.count( "/" ) +1;
     QAction* parentAction = 0;
     QString subPath;
-    
+
     for ( int i = 0; i < separatorCount; i++ ) {
         subPath = path.section( '/', 0, i );
         action = mActions.value( subPath );
-        
+
         if ( action ) {
             if ( path != subPath ) {
                 continue;
             }
-            
+
             return action->menu() ? action : 0;
         }
-        
+
         parentAction = mActions.value( i == 0 ? QString::null : path.section( '/', 0, i -1 ) );
         const int row = children( parentAction ).count();
         action = (new QMenu)->menuAction();
         action->setText( path.section( '/', i, i ) );
         insertAction( subPath, action, parentAction, row );
     }
-    
+
     return action;
 }
 
@@ -546,11 +546,11 @@ void pActionsModel::removeCompleteEmptyPathNode( QAction* action )
     if ( !action || !mActions.contains( path( action ) ) ) {
         return;
     }
-    
+
     if ( !hasChildren( action ) ) {
         QAction* parentAction = parent( action );
         const int row = children( parentAction ).indexOf( action );
-        
+
         removeAction( action, parentAction, row );
         removeCompleteEmptyPathNode( parentAction );
     }
@@ -559,7 +559,7 @@ void pActionsModel::removeCompleteEmptyPathNode( QAction* action )
 void pActionsModel::actionChanged()
 {
     QAction* action = qobject_cast<QAction*>( sender() );
-    
+
     if ( action ) {
         emit dataChanged( index( action, 0 ), index( action, mColumnCount -1 ) );
         emit actionChanged( action );
@@ -570,7 +570,7 @@ void pActionsModel::actionDestroyed( QObject* object )
 {
     QAction* action = (QAction*)object;
     QString path = this->path( action );
-    
+
     if ( mActions.contains( path ) ) {
         removeAction( path );
     }
